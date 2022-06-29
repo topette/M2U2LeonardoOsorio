90d3b1ee37408cbf0308b0ff4cd1ee0253fbfb45
topette <oscarleonardo.osorio@gmail.com>
2022-06-28 21:14:08 -0500

    creando inputs e instanciando

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //Archivo de JS
+
+const a = document.getElementById("valorA")
+const b = document.getElementById("valorB")
+const c = document.getElementById("valorC")
+const d = document.getElementById("valorD")
+const e = document.getElementById("valorE")
+const enviar1 = document.getElementById("enviar1")
